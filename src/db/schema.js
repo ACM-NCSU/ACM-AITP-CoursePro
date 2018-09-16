dde2cd06a9cbc9f291c5752e9a025b94cb29180e
wpmoore2 <wpmoore2@ncsu.edu>
2018-09-16 16:01:09 -0400

    Notes DB Schema

--- src/db/schema.js
+++ src/db/schema.js
@@ -1,16 +1,24 @@
 'use strict';
 
 // Table Names
-var TABLE_NAMES = {WORKSPACES: 'WORKSPACES'};
+var TABLE_NAMES = {WORKSPACES: 'WORKSPACES',
+                   NOTES:      'NOTES'};
 
 // Table Definitions (command used to create table)
-var WORKSPACES_DEF = "CREATE TABLE IF NOT EXISTS " + TABLE_NAMES.WORKSPACES +
+var WORKSPACES_DEF = `CREATE TABLE IF NOT EXISTS ${TABLE_NAMES.WORKSPACES}` +
                      " ( name        text         PRIMARY KEY, " +
                      "   active      boolean      DEFAULT false," +
                      "   last_used   boolean      DEFAULT false);";
 
+var NOTES_DEF = `CREATE TABLE IF NOT EXISTS ${TABLE_NAMES.NOTES}` +
+                     " ( name        text         PRIMARY KEY, " +
+                     "   workspace   text," +
+                     "   location    text," +
+                     `      FOREIGN KEY (workspace) REFERENCES ${TABLE_NAMES.WORKSPACES}(name));`;
 
-var TABLE_DEFS = {WORKSPACES: WORKSPACES_DEF};
+
+var TABLE_DEFS = {WORKSPACES: WORKSPACES_DEF,
+                  NOTES: NOTES_DEF};
 
 
 module.exports.TABLE_NAMES = TABLE_NAMES;
